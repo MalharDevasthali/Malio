7deac0aa5b3a62b4469ea79116550287530342c5
Malhar <malhardevasthali007@gmail.com>
2023-11-05 12:26:38 +0530

    Added Common Scripts

---
+++ Assets/Scripts/Commons/GameService.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Player;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Commons
+{
+    public class GameService : GenericMonoSingleton<GameService>
+    {
+        [SerializeField] private PlayerService playerService;
+
+        private void Start()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Commons/GenericMonoSingleton.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Assets.Scripts.Commons
+{
+    public class GenericMonoSingleton<T> : MonoBehaviour where T : GenericMonoSingleton<T>
+    {
+        private static T Instance;
+        public static T instance { get { return Instance; } }
+
+        protected virtual void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = (T)this;
+            }
+            else
+            {
+                Debug.LogError(Instance + "is Tring to create another instance");
+                Instance = null;
+            }
+        }
+    }
+}
